83cffdcce91a7bf3e85d549a12844d27e913948d
Robert Alm <rdodd@Roberts-MacBook-Pro.local>
2026-01-30 13:24:18 -0500

    plotter

--- gui/src/main.rs
+++ gui/src/main.rs
@@ -1,4 +1,5 @@
 use eframe::egui;
+use egui_plot;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
@@ -31,6 +32,20 @@ struct MyApp {
     time: u32,
     status: Status,
     heat: Heat,
+    graph: Graph,
+}
+
+#[derive(Clone, Copy, PartialEq)]
+pub struct Graph {
+    line_style: egui_plot::LineStyle,
+}
+
+impl Default for Graph {
+    fn default() -> Self {
+        Self {
+            line_style: egui_plot::LineStyle::Solid,
+        }
+    }
 }
 
 impl MyApp {
@@ -41,6 +56,17 @@ impl MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        let data: [[f64; 2]; 6] = [
+            [0.0, 20.0],
+            [1.0, 100.0],
+            [2.0, 400.0],
+            [3.0, 800.0],
+            [4.0, 1100.0],
+            [5.0, 1200.0],
+        ];
+
+        let points = egui_plot::PlotPoints::from(data.to_vec());
+        let line = egui_plot::Line::new("Points", points);
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Test:");
             ui.add(
@@ -52,6 +78,12 @@ impl eframe::App for MyApp {
             if ui.button("Start Test").clicked() {
                 self.status = Status::Running;
             }
+
+            egui_plot::Plot::new("temperature_plot")
+                .height(300.0)
+                .show(ui, |plot_ui| {
+                    plot_ui.line(line);
+                });
         });
     }
 }
